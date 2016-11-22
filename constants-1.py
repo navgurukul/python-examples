e20baf23814fd1487b02b5e95a3bda4b37ba4c05
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2016-11-23 02:37:34 +0530

    constants exercise complete

---
+++ constants-1.py
@@ -0,0 +1,32 @@
+# Aaiye Constants ko samajhte hai
+
+# Constant matlab jo badalta nahi hai
+
+# Jaise numbers, letters, strings ko hum constants bolte hai
+
+
+# 123 ek number hai, jo 123 hi rahega, isliye constant
+
+# numbers ko python mei INTEGER bolte hai
+
+print 123
+
+# 9.9 ek number hai jisme decimal yani decimal number (dashamlav ke saath), jo 9.9 hi rahega, isliye constant
+
+# aise numbers ko python mei FLOAT bolte hai
+
+print 9.9
+print 7.8
+
+# python mei kisi bhi text ko STRING bolte hai
+
+# jaise "hello world" ek STRING hai
+
+# "hello world" "hello world" hi rahega, isliye constant
+
+# String ko " " (yaani double quotes) ya ' ' (yaani single quotes) ke beech mei daala jata hai
+
+# e.g.
+
+print 'hello world'
+print 'navgurukul'
---
+++ constants-a.py
@@ -0,0 +1,33 @@
+# Yeh exercise hai
+
+# har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+
+# aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+# question 1
+# kuch STRINGS ko print karein. aapko jo man kare aap un STRINGS ko print kar sakte hai
+
+
+
+
+
+
+# question 2
+# kuch FLOATS ko print karein, aapko jo man karein aap woh FLOATS print kar sakte hai
+
+
+
+
+
+
+# question 3
+# kuch INTEGERS ko print karein, aapko jo man karein aap woh INTEGERS print kar sakte hai
+
+
+
+
+
+
+
+# question 4
+# constants kya hote hai, apna answer comments mei likhe
