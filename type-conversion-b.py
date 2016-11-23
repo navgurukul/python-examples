c15e65581ea088191c119c39bb3abf6a3f21a1ba
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2016-11-23 11:05:13 +0530

    type conversion assignment b

---
+++ type-conversion-b.py
@@ -0,0 +1,41 @@
+# Yeh exercise hai
+# har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+
+# var1 mei user se ek STRING input lijiye
+
+
+
+
+
+
+# var2 mei user se ek STRING input lijiye
+
+
+
+
+
+
+# var1 aur var2 ko jod kar print kijiye
+
+
+
+
+
+
+# vara mei user se ek INTEGER input lijiye
+
+
+
+
+
+
+# varb mei user se ek INTEGER input lijiye
+
+
+
+
+
+
+# vara aur varb ka sum print kijiye
