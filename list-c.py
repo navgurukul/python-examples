0d80296b1040258f66bd18519b652d2ea8a8d9f2
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2016-11-29 09:55:19 +0530

    list c exercise complete

--- list-c.py
+++ list-c.py
@@ -3,5 +3,60 @@
 # Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
 
 # Aap apni questions_list ko iterate karein
+# Har iteration mei kuch aisa print karein
+#
+# ----------------------------------------
+# <question text e.g - Python kya hai?>
+# ----------------------------------------
+# 
+# yeh dashes bhi print karne hai
+
+
+
+
+
+
+# Uppar wale code ko modify karein jisse, woh har iteration ke baad user se input le, ki user ko kya lagta hai kya answer hai
+#
+# ----------------------------------------
+# <question text e.g - Python kya hai?>
+# ----------------------------------------
+# 
+# Iska Answer (1, 2, 3 and 4 mei kariye)
+# <yaha par user ka input le>
+#
+
+
+
+
+
+
+# Aap apni questions_list ko iterate karein
 # Har iteration mei user se input lo, ki user ko kya lagta hai kya answer hai
+
+
+
+
+
+
+# Aap apni questions_list ko iterate karo
+# Har iteration par question ki length nikaal kar ek total_length naam mei append karo
+# Matlab agar question "Python kya hai?" toh length 15 add hogi total_length mei
+# total_length variable ko print karo
+
+
+
+
+
+
+
+# Aap apni questions_list ko iterate karo
+# Uppar wale question mei change karo ki agar question ki length 15 se kam hai toh print karein
+# "Question chota hai"
+# Nahi toh likho "Question kaafi lamba hai"
+
+
+
+
+
 
