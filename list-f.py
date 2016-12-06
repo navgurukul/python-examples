a12303e0df8b067e7ecf311f88507f3d8ba283e5
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2016-12-06 10:13:12 +0530

    added some more examples

---
+++ list-f.py
@@ -0,0 +1,41 @@
+# Yeh exercise hai
+# Har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+
+# Yeh example dekho
+
+num_list = range(100)
+num_squares_list = []
+for ele in num_list:
+	num_squares_list.append(ele*ele)
+
+print num_squares_list
+
+
+# Iss example mei humne ek list banayi, aur list ko iterate kiye
+# Iterate karte hue har element ko modify karke humne ek nayi list num_squares_list mei daal diya
+
+
+# Ab aisa hi ek program likhe jo numbers ko 10 se multiply karke ek nayi list mei daalti hai
+
+
+
+
+
+
+
+
+
+list1 = [130, 131, 132, 133, 134, 135, 136, 137, 138, 139]
+list2 = [245, 246, 247, 248, 249, 250, 251, 252, 253, 254]
+
+# ab ek nayi list list3 banao jismei har ek element corresponding list1 and list2 ke elements ka sum hai
+
+
+
+
+
+
+
+
---
+++ variables-c.py
@@ -0,0 +1,10 @@
+# Yeh exercise hai
+
+# har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+
+# aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+a = 5
+b = 7
+
+# Yaha par kuch aisa code likhe jisse ki a = 7 aur b ko 5 
