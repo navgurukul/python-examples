819bdfb99c63188d3311f1ff346b617c1fcd755f
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2016-12-07 08:58:18 +0530

    added booleans assignment

---
+++ booleans-1.py
@@ -0,0 +1,48 @@
+# Booleans special type ke constants hote hai (humne abhi tak Strings, Integers aur Floats ke baarein mei padha tha).
+
+# Do hi Boolean values hoti hai - True aur False.
+
+# Jaise integers ko add, subtract, divide, multiply, etc. kiya ja sakta hai (isko operation kehte hai - jaise integers par add operation).
+
+# Aise hi Booleans par bhi kuch special operations kiye ja sakte hai. Iske liye, yeh example and yeh exercise karo.
+
+
+# Do assignments possible hai - 
+
+pehlaBool = True
+doosraBool = False
+
+
+# Important Boolean operations  - and, or, not
+
+# AND
+
+# And kya matlab hota hai - AUR
+# Aisa socho agar ki agar hum
+
+# pehlaBool and doosraBool likhenge toh and operator pehlaBool and secondBool par kaam karega
+# Iss ka result True hoga agar dono hi Boolean values True hongi. Agar ek bhi value false ho jayegi, toh "and" operator false return kar deta hai
+
+print (True and True
+print (True and False)
+print (False and True)
+print (False and False)
+
+# Isko aise bhi soch sakte hai ki agar
+# pehlaBool and doosraBool True hai toh result True hai
+
+
+# Aise hi Or ko socho
+# pehlaBool or (yaani ya) doosraBool True hai toh result True hai
+
+print (True or True
+print (True or False)
+print (False or True)
+print (False or False)
+
+# Not toh sabse simple hai
+
+print (not True)
+print (not False)
+
+# Not sirf ek hi value par kaam karta hai, and usko ulta kar deta hai. True ko False, or False ko True
---
+++ booleans-a.py
@@ -0,0 +1,22 @@
+# Yeh exercise hai
+
+# har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+
+# aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+pehlaBool = True
+doosraBool = False
+
+# In commands ke output pehle socho, and phir python shell mei daal kar dekho agar woh theek hai. Agar woh theek hai toh theek, nahi toh wapas se exercise padh kar samjho ki aapka answer sahi kaise ho sakta tha?
+
+
+print pehlaBool and doosraBool
+print pehlaBool or doosraBool
+print not pehlaBool
+print not doosraBool
+
+
+print (pehlaBool and doosraBool)
+print (pehlaBool or doosraBool)
+print (not pehlaBool)
+print (not doosraBool)
