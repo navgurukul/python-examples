b8695d90f669ef7cde15f0cfc42dbca28177d945
Rishabh Verma <me@rishabhverma.me>
2016-12-12 23:59:58 +0530

    functions examples & exercise 1

---
+++ functions-1.py
@@ -0,0 +1,58 @@
+# Inn examples se hum FUNCTIONS samjhenge.
+# Note: Examples samajhte hue lightbot ke functions ko dimaag mein rakhna.
+
+# FUNCTIONS ka use kar ke hum python mein ek baar code likh kar usko baar baar istamaal kar sakte hain.
+# Hum ek function likhenge. Iss code ko pehle chala ke dekho, aur fir neeche padiye iske baare mein samajhne ke liye.
+
+def function_say_hello():
+    print "Hello"
+    print "Main function ke andar se print ho raha hun."
+function_say_hello()
+print "NavGurukul delhi mein hai."
+function_say_hello()
+
+# Shuruat mein "def" ka use kar ke hum python ko bata rahe ki humne ek "function_say_hello" naam ka function likha hai.
+# Iss function ka code (do print karne ki lines) kuch indent (space) deke "def function_say_hello()"" ke neeche likha hua hai. Inn spaces se python samajh jata hai ki yeh function ka code hai.
+# Fir agli line, "function_say_hello()" mein humne function ko bulaya hai jisse function ke andar wala code chal jata hai. Isko "FUNCTION CALL" karna kehte hain.
+# Iske baad print statement use kar ke humne ek string print kiya hai.
+# Aakhir mein humne firse "function_say_hello()" likh ke function call ki hai jo ek aur baar function ek andar wale code ko chala deti hai jisse andar waali 2 print ki command chal jati hai.
+# Yahan yeh dekho ki kaise "NavGurukul delhi mein hai" ke upar aur neeche "unction_say_hello()" ke andar wali print command ke string print hue hain.
+
+
+
+
+# Ek aur example FUNCTIONS samajhne ke liye
+# Iss code ko padh ke output socho. Chala ke dekhne ke baad dhyan se padho.
+
+def function_say_bye():
+    print "Aapko mil ke maza aaya. "
+    print "Bye bye"
+function_say_bye()
+function_say_bye()
+print "Python ka istamaal bahot jagah hota hai."
+function_say_bye()
+function_say_bye()
+
+# Hum python mein "def" keyword ka use kar ke function banate hain. def ke baad function ka naam, "function_say_bye" likha hain. Naam ke baad hum do bracket ( ) lagate hain.
+# Bracket ke baad ek semicolon, : lagate hain. Python semicolon se samajh jaati hai ki function ka code shuru ho raha hai.
+# Dekhiye ki sara function ka code indent (space) deke likha hua hai. Isse python ko samajh aata hai ki yeh function ke andar ka code hai.
+# print "Bye bye" ke baad code space diye bina likha hua hai. Isse python ko samajh aati hai ki function ka code kahatam ho gaya hai
+# functions ko bhi variables ko naam dene ke rules ke hisaab se hi naam dete hain. yaad rakhne ke liye ek baar variables ko naam dene ke rules yaad rakh li jiye.
+
+
+
+# FUNCTION CALLS ko samajhna
+# Neeche diye gaye code ko likhiye aur dekhiye ki iska koi output aa raha hai ya nahi. Nahi aa raha toh kyun nahi aa rahi?
+
+def function_say_hello_again():
+    print "Firse Hello :)"
+    print "Aap kaise ho?"
+
+
+# Ab apne code mein yeh line add karein aur dekhein ki koi output aati hai.
+function_say_hello_again()
+
+# Python mein sirf function likh dene se function ka code chalta nahi hai. Yahan pehle humne def keyword ka use karke python ko function ka code bataya.
+# Isko "FUNCTION DEFINITION" kehte hain.
+# Fir humne baad mein "function_say_hello_again()" likh ke function ko bulaya aur usme likha hua code chalwaya. Isko "FUNCTION CALL" karna kehte hain.
+# "FUNCTION CALL" mein hum function ke naam ke baad 2 bracket ( ) lagate hain. Isse python ko pata chalta hai ki function ke andar ka code chalana hai
---
+++ functions-a.py
@@ -0,0 +1,49 @@
+# Yeh exercise hai
+# Har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+# ask_question naam ka Ek function likhiye jo yeh text ko "ek baar" print kare.
+# Fir iss function ko 5 baar call kar ke yeh text 5 baar print karvao
+"""
+---------------------------------------------------------------------------
+Who is the founder of Facebook?
+Mark Zuckerberg
+Bill Gates
+Steve Jobs
+Larry Page
+---------------------------------------------------------------------------
+"""
+
+
+
+
+
+
+
+
+
+
+
+# function_one naam ka function likhiye jo koi bhi 2 lines print karta hai
+# saath saath, function_two naam ka ek function likhiye jo bhi koi dusri 2 lines print karta hai
+# ab aisa code likhiye ki pehle "function_two ki output" print hoye, fir "function_one ki output", aur fir "function_two ki output" firse print hoye
+
+
+
+
+
+
+
+
+
+
+# range ka use karke ek for loop likhiye aur iss for loop ke andar pehli exercise wala ask_question ko 10 baar call karein
+
+
+
+
+
+
+
+
+ 
