77b29bfc89f5bf24f91ef9a79c2bc10c00cb0fc8
Rishabh Verma <me@rishabhverma.me>
2016-12-14 17:53:11 +0530

    functions second exercise

---
+++ functions-b.py
@@ -0,0 +1,71 @@
+# Yeh exercise hai
+# Har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+# function_print_lines naam ka ek function likho jo 2 strings leta ho, aur uske baad unn do strings ko aise print karta ho. Jaise agar main usko
+# "Mera naam Rishabh hai." aur "Main NavGurukul ka co-Founder hun." dunga toh woh yeh print karega:
+'''
+------------------------------
+Mera naam Rishabh hai.
+Main NavGurukul ka co-founder hun.
+------------------------------
+'''
+# iske baad ek function_print_again naam ka function banaye jiss function mein function_print_lines 3 baar call hoye aur jo bhi
+# isme yeh dhyaan rakhiye ki jo arguments hum function_print_again ko call karte hue denge woh hi 3 baar print hone chaiye
+# ab function_print_again kisi bhi do strings ke saath call kare
+
+
+
+
+
+
+
+
+# add_numbers naam ka function likhiye jo do numbers ko arguments ke tarah le aur fir unka sum print karta hai. Agar main uss function ko 56 aur 12 dunga toh woh aise output dega:
+'''
+---------------
+68
+---------------
+'''
+
+
+
+
+
+
+
+
+# add_numers_list naam ka function likhiye jo do integers ki 2 lists leta ho aur fir same position wale integers ka sum print karta ho. woh integers ka sum aapka pichla function add_numbers use karke karega.
+# jaise agar hum iss function ko [50, 60, 10] aur [10, 20, 13] denge ko woh yeh print karega:
+'''
+---------------
+60
+---------------
+---------------
+80
+---------------
+---------------
+23
+---------------
+'''
+# hum jo iss number ko lists denge woh lists kitni bhi lambi ho sakti hain
+
+
+
+
+
+
+
+# check_numbers naam ka ek function likhiye jo do numbers le aur fir print kare "Dono even hain" agar dono numbers even (2 se divide hote hain) nahi toh print kare "Dono numbers even nahi hai"
+# Yahan tumhe and operator aur if statement ka use karna padega. Agar bhul gaye ho toh wapas jake padh sakte hain
+
+
+
+
+
+
+
+
+
+# jo aapne add_numers_list wala function likha hai usko yahan copy-paste karein.
+# fir usko change karein aur dono numbers ke sum ke saath saath check_numbers ka use karke yeh bhi print karvayein ki woh dono numbers even hai ya nahi hai.
