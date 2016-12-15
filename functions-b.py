681173b18093a52f770698ed4650baa5fd77c9ca
Rishabh Verma <me@rishabhverma.me>
2016-12-15 10:53:27 +0530

    Better explaintion for a few exercises

--- functions-b.py
+++ functions-b.py
@@ -34,7 +34,7 @@ Main NavGurukul ka co-founder hun.
 
 
 
-# add_numers_list naam ka function likhiye jo do integers ki 2 lists leta ho aur fir same position wale integers ka sum print karta ho. woh integers ka sum aapka pichla function add_numbers use karke karega.
+# add_numbers_list naam ka function likhiye jo do integers ki 2 lists leta ho aur fir same position wale integers ka sum print karta ho. woh integers ka sum aapka pichla function add_numbers use karke karega.
 # jaise agar hum iss function ko [50, 60, 10] aur [10, 20, 13] denge ko woh yeh print karega:
 '''
 ---------------
@@ -47,7 +47,8 @@ Main NavGurukul ka co-founder hun.
 23
 ---------------
 '''
-# hum jo iss number ko lists denge woh lists kitni bhi lambi ho sakti hain
+# hum jo iss number ko lists denge woh lists kitni bhi lambi ho sakti hain. toh aapne aisa loop likhna hai ji jitni bhi lambi list pe iterate kar sakta hai.
+# Apni list waali exercises yaad karo ki yeh kaise karte hai. Abhi ke liye aapko sirf 3 items ki list ke liye hi yeh likhna hai
 
 
 
@@ -67,4 +68,6 @@ Main NavGurukul ka co-founder hun.
 
 
 # jo aapne add_numers_list wala function likha hai usko yahan copy-paste karein.
-# fir usko change karein aur dono numbers ke sum ke saath saath check_numbers ka use karke yeh bhi print karvayein ki woh dono numbers even hai ya nahi hai.
+# jaise aapne list ke same index number wale dono numbers ko add_numers mein argument deke add kiya tha ussi tareeke
+# se same index numbers wale list items ko check_numbers ko arguments deke yeh print karvao ki dono even hai ya nahi=
+# isko karne ke liye aap jo do list ke same index wale numbers add_numbers ko argument mein dete ho, wahi do numbers check_numbers ko bhi de sakte ho.
