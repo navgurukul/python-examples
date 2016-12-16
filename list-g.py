1fbb18eac7d42c9d32887fc6624b506dae380c35
Rishabh Verma <me@rishabhverma.me>
2016-12-17 01:53:13 +0530

    1st general exercise.

--- list-g.py
+++
@@ -1,3 +0,0 @@
-# Yeh exercise hai
-# Har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
-# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
