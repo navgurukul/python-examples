a4a25a5e029c7331cd90d8eee45424f0c6c0dc78
Rishabh Verma <me@rishabhverma.me>
2016-12-17 02:54:15 +0530

    2nd general exercise.

---
+++ general-exercise-2.py
@@ -0,0 +1,140 @@
+# Yeh exercise hai
+# Har line of code se pehle aapko ek line mei comment  mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+# Pehle ke variable_list mein 0 se 100 integers ki list banayein
+# Fir aapne iss list pe iterate karte hue 3 nayi list banani hai:
+# - Pehli list mein variable_list ke items ki 3 se multiply karke jo result aayta hai, woh hona chaiye
+# - Dusri list mein variable_list ke items ko 4 se divide karke jo result aata hai, woh hona chaiye.
+#   Iss dusri list mein saari items `float` honi chaiye
+# - Teesri list variable_list ke items ko "NavGurukul" string ke saath jodna hai. Jaise:
+#   Agar variable_list ka pehla item 0 hai, iss teesri list ka pehla item "NavGurukul0" hona chaiye
+#   Dusra item "NavGurukul1"
+#   Teesra item "NavGurukul2"
+# Iss program mein bas list banani hai. Kuch print nahi karna.
+
+
+
+
+
+
+
+
+
+
+# - Ek number ka factorial 0 se leke uss number tak ke saare numbers ko ek saath multiply karke nikalta hai.
+# Jaise 3 ka factorial 6 hai. Kyunki -
+# 1 * 2 * 3 ko calculate karke 6 aata hai
+# 4 ka factorial 24 hai. Kyunki -
+# 1 * 2 * 3 * 4 ko calculate karke 24 aata hai
+# Aise hi 7 ka factorial 5040 hai. Kyunki -
+# 1 * 2 * 3 * 4 * 5 * 6 * 7 ko calculate karke 5040 aata hai
+# Ab aap ek program likhoge jo ki user se ek integer input lega aur fir uska factorial print karega.
+# Agar user 3 dalega ko 6 print karega, 7 dalega toh 5040 print karega aur aise hi dusre numbers ke lie.
+# Note: Abhi ke liye yeh soch lo ki user sirf positive integers hi dalega. Negative integers kabhi nahi dalega.
+
+
+
+
+
+
+
+
+
+
+# numbers_list naam ki ek list banayein jisme 1 se 10000 (das hazar) tak integers hon
+# Ab numbers_list ke saare numbers ka sum nikalne ka code likhein.
+# End mein sum ko print karein
+
+
+
+
+
+
+
+
+
+
+# Socho aapko paas ek list hai jisme kuch values baar baar aa rahi hain.
+# Ek aisa code likho jisse aap ek nayi list banayein jisme iss list ki items ek ek baar hi aaye.
+# Jaise:
+# string_list = ["Rishabh", "Rishabh", "Abhishek", "Rishabh", "Divyashish", "Divyashish"]
+# Aapke code ko iss string_list se ek nayi list banani chaiye jo yeh hogi:
+# new_list = ["Rishabh", "Abhishek", "Divyashish"]
+# Yeh list dekhiye isme saare naam ek ek baar aa rahe hain. Farak nahi padta ki pichli list mein kitne baar aa rahe the.
+# Samajhne ke liye ek aur example padho
+# string_list = ["Delhi", "Delhi", "Mumbai", "Mumbai", "Delhi", "Chennai", 'Chennai']
+# Isse aapke code ko yeh nayi list banani hogi:
+# new_list = ["Delhi", "Mumbai", "Chennai"]
+# Iss nayi list mein sirf saare shehron ka naam sirf ek baar aa raha hai.
+# Yeh rahi aapki pehli items repeat hone waali list:
+string_list = ["Empathy", "Empathy", "Kindness", "Kindness", "Compassion", "Humble", "Humble"]
+
+
+
+
+
+
+
+
+
+
+# Socho aapke paas 2 lists hain. Aapne aisa code likhna hain jisse ek nayi list banne
+# jisme inn dono lists ke woh item hone chaiye ho dono list mein aa rahe hain.
+# Socho aapki do list yeh hain:
+# list1 = [1, 342, 75, 23, 98]
+# list2 = [75, 23, 98, 12, 78, 10, 1]
+# Inn dono list se aapki nayi list yeh banni chaiye:
+# new_list = [1, 75, 98]
+# Iss nayi list mein sirf 1, 75 aur 98 isliye hain kyunki aur koi bhi items dono lists mein nahi aa rahi.
+# Dusri saari items ya toh pehli list mein aa rahi hai ya dusri mein.
+# Note: Aapka yeh code kitne bhi items ki list pe kaam karna chaiye.
+
+
+
+
+
+
+
+
+
+
+
+
+# Maan li jiye aapke paas yeh kuch english words ki list hai:
+words = ['time',  'person',  'year',  'way',  'day',  'thing',  'man',  'world',  'life',  'hand',  'part',  'child',  'eye',  'woman',  'place', 'case',  'point',  'government']
+# Aapne aisa code likhna hai jo do cheezein kare:
+# - Saare unn words ka count print karo jinme "e" aata hai
+# - Saare unn words ka count print karo jinme "i" aata hai
+
+
+
+
+
+
+
+
+
+
+# - Socho aapke paas do lists hain. Ab aapko nayi list banani hai jisme dono lists ke elements hone chaiye.
+# Lekin yeh dhyan mein rakhna hai ki dono lists ke saare elements sirf ek-ek baar hi hone chaiye.
+# Agar humare paas yeh do lists hain:
+# list1 = [1, 5, 10, 12, 16, 20]
+# list2 = [1, 2, 10, 13, 16]
+# Toh humari nayi list yeh hogi:
+# new_list = [1, 2, 5, 10, 12, 13, 16, 20]
+# - Yahan dekho ki dono lists ke items ek ek baar aa rahe hain.
+# - Jaise dono lists mein 1 aa raha tha, lekin nayi list mein ek hi baar aa raha hai.
+# - Aise hi 10 aur 16 bhi dono list mein aa raha tha, lekin nayi list mein ek hi baar hai
+# - Aur 5, 2, 12, 13 aur 20 mein se kuch pehli list mein the aur kuch dusri mein, lekin
+#   sabhi nayi list mein ek ek baar aa rahe hain
+
+
+
+
+
+
+
+
+
+ 
