ac1d75d6918b9f082580b7552b33d77f3a340c28
Rishabh Verma <me@rishabhverma.me>
2016-12-17 03:00:04 +0530

    Adding exercise 3 too though just the raw skeleton is ready until now.

---
+++ general-exercise-3.py
@@ -0,0 +1,39 @@
+
+# lists 2
+# - reverse a list
+# - write fibonaci series
+# - maximum string length in a list
+# - check whether something is palindrome or not
+# - guessing game
+
+
+
+
+
+# lists & iteration
+# - general proficency
+# - if else statements within loops
+# - creating variables within loops
+# - breaking out of loops
+# - popping out
+
+
+# nested lists & if statements
+# - frequency listing and histogram printing in a list
+
+
+# - sum of largest and smallest values of list
+
+# set operations
+# raw input text and see if that function is a vowel
+# number guessing game
+# sum of all elements of an array
+# list intersection
+# reverse an array
+# check palindrome
+# remove duplicates in a list
+# frequency listing of a list
+# histogram of a list
+# write the fibonaci series
+# frequency of a particular word in a list
+# frequency of any given character in a list thru a function
