e10e7fd49b527f16be22b3a2b82195ac6b145145
Rishabh Verma <me@rishabhverma.me>
2016-12-17 11:46:37 +0530

    3rd exercise almost done.

--- general-exercise-3.py
+++ general-exercise-3.py
@@ -1,39 +1,78 @@
+# Yeh exercise hai
+# Har line of code se pehle aapko ek line mei comment  mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
 
-# lists 2
-# - reverse a list
-# - write fibonaci series
-# - maximum string length in a list
-# - check whether something is palindrome or not
-# - guessing game
-
-
-
-
-
-# lists & iteration
-# - general proficency
-# - if else statements within loops
-# - creating variables within loops
-# - breaking out of loops
-# - popping out
+# Ek code likho jo ek list ko ulta kar ke ek nay list banaye.
+# Jaise agar aapke paas yeh list hai:
+# numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Aapke code run karne ke baad nayi list aisi honi chaiye:
+# numbers_list = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 
-# nested lists & if statements
-# - frequency listing and histogram printing in a list
 
 
-# - sum of largest and smallest values of list
 
-# set operations
-# raw input text and see if that function is a vowel
-# number guessing game
-# sum of all elements of an array
-# list intersection
-# reverse an array
-# check palindrome
-# remove duplicates in a list
-# frequency listing of a list
-# histogram of a list
-# write the fibonaci series
-# frequency of a particular word in a list
-# frequency of any given character in a list thru a function
+
+
+
+
+# Fibonacci series ek numbers ki vishesh vyavastha (special arrangement) hai. Yeh kuch aisi hoti hai:
+# 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ...
+# Yahan har number pichle do numbers ka sum (jod) hota hai. Jaise
+# - 2 humein 1 + 1 kar ke milega.
+# - 3 humein 2 + 1 kar ke milega
+# - 5 humein 3 + 2 kar ke milega
+# - 8 humein 5 + 3 kar ke milega
+# - 13 humein 8 + 5 kar ke milega
+# - 21 humein 13 + 8 kar ke milega
+# Iss series meim pehle do numbers (1 aur 1) ko chor ke saare number pichle do numbers ka sum hain
+# Ek python program likho jo pehle 100 fibonacci series ke number print kare
+
+
+
+
+
+
+
+
+
+
+# Ek list mein se sabse bade number ko print karne ka code likho
+# Jaise agar yeh list hai:
+# numbers_list = [1,2,3,4,5,200,12, 12, 30, 533, 32]
+# Toh aapke code ko 533 print karna padega kyunki woh sabse bada hai is list ke saare numbers mein se.
+# Yeh list hai lekin aapka code kisi bhi list pe kaam karna chaiye. Aur list ki length se bhi farak nahi padna chaiye:
+numbers_list = [65, 134, 23245, 311, 213, 953, 32, 674, 21, 3653]
+
+
+
+
+
+
+
+
+
+
+# Palindromes woh shabd hote hain jo aage se likho ya piche se likho same hi hoga
+# Jaise "madam" word aage se likho toh bhi same hai, aur piche se bhi#
+# Aise hi "kayak" bhi ek palindrome hai
+# Aise hi "eye" bhi ek palindrome hai
+# "level" bhi ek palindrome hai
+# Iss exercise ke liye aapko ek list denge jisme kuch words diye honge. Jaise:
+# string_list = ['k', 'a', 'y', 'a', 'k']
+# Aapne yeh check karna hai ki yeh list ek palindrome hai ya nahi. Yeh karne ke liye aapko
+# dekhna padega ki aage se aur piche se dono jagah se list ki item same kram mein hain ya nahi
+# Agar woh list palindrom hai toh print karein "Yes list is a plaindrome"
+# Nahi toh print karo "No not a palindrome"
+
+
+
+
+
+
+
+
+
+# guessing game
+# http://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html
+# @Abhishek: You will have to introduce random.randint before this. 
