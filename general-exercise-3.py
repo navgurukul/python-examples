0fee035b23e9b9222ec2596fbc60f1b6f4bef304
Rishabh Verma <me@rishabhverma.me>
2017-01-02 20:00:03 +0530

    Split up some extra long exercises.

--- general-exercise-3.py
+++ general-exercise-3.py
@@ -43,36 +43,3 @@
 # Toh aapke code ko 533 print karna padega kyunki woh sabse bada hai is list ke saare numbers mein se.
 # Yeh list hai lekin aapka code kisi bhi list pe kaam karna chaiye. Aur list ki length se bhi farak nahi padna chaiye:
 numbers_list = [65, 134, 23245, 311, 213, 953, 32, 674, 21, 3653]
-
-
-
-
-
-
-
-
-
-
-# Palindromes woh shabd hote hain jo aage se likho ya piche se likho same hi hoga
-# Jaise "madam" word aage se likho toh bhi same hai, aur piche se bhi#
-# Aise hi "kayak" bhi ek palindrome hai
-# Aise hi "eye" bhi ek palindrome hai
-# "level" bhi ek palindrome hai
-# Iss exercise ke liye aapko ek list denge jisme kuch words diye honge. Jaise:
-# string_list = ['k', 'a', 'y', 'a', 'k']
-# Aapne yeh check karna hai ki yeh list ek palindrome hai ya nahi. Yeh karne ke liye aapko
-# dekhna padega ki aage se aur piche se dono jagah se list ki item same kram mein hain ya nahi
-# Agar woh list palindrom hai toh print karein "Yes list is a plaindrome"
-# Nahi toh print karo "No not a palindrome"
-
-
-
-
-
-
-
-
-
-# guessing game
-# http://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html
-# @Abhishek: You will have to introduce random.randint before this. 
---
+++ general-exercise-4.py
@@ -0,0 +1,29 @@
+# Yeh exercise hai
+# Har line of code se pehle aapko ek line mei comment  mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+
+
+# Palindromes woh shabd hote hain jo aage se likho ya piche se likho same hi hoga
+# Jaise "madam" word aage se likho toh bhi same hai, aur piche se bhi#
+# Aise hi "kayak" bhi ek palindrome hai
+# Aise hi "eye" bhi ek palindrome hai
+# "level" bhi ek palindrome hai
+# Iss exercise ke liye aapko ek list denge jisme kuch words diye honge. Jaise:
+# string_list = ['k', 'a', 'y', 'a', 'k']
+# Aapne yeh check karna hai ki yeh list ek palindrome hai ya nahi. Yeh karne ke liye aapko
+# dekhna padega ki aage se aur piche se dono jagah se list ki item same kram mein hain ya nahi
+# Agar woh list palindrom hai toh print karein "Yes list is a plaindrome"
+# Nahi toh print karo "No not a palindrome"
+
+
+
+
+
+
+
+
+
+# guessing game
+# http://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html
+# @Abhishek: You will have to introduce random.randint before this.
