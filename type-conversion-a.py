f356b9df5e37d3258138da020cf14f6dc042fc28
vidur <vidur@navgurukul.org>
2017-01-06 16:56:58 +0530

    moified type-conversion-a.py

--- type-conversion-a.py
+++ type-conversion-a.py
@@ -2,31 +2,36 @@
 # Har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
 # Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
 
-#var12 mei ek STRING value daalkar usse FLOAT mei type cast karo
+#var12 mei ek STRING value daalni hai
+#fir var13 mein uss STRING value ko FLOAT mei typecast karo
 
 
 
 
 
-#var23 mei ek FLOAT value daalkar usse INTEGER mei type cast karo
-
-
-
-
-
-
-#var34 mei ek INTEGER value daalkar usse STRING mei type cast karo
-
-
-
-
-
-
-#var45 mei ek FLOAT value daalkar usse STRING mei type cast karo
+#var23 mei ek FLOAT value daalni hai
+#fir var24 mein uss FLOAT value ko INTEGER mei type cast karo
 
 
 
 
 
 
-#var56 me ek STRING value daalkar usse INTEGER mein type cast kare
+#var34 mei ek INTEGER value daalni
+#fir var35 me uss INTEGER value ko STRING mei type cast karo
+
+
+
+
+
+
+#var45 mei ek FLOAT value daalni hai
+#fir var46 mei uss FLOAT value ko STRING mei type cast karo
+
+
+
+
+
+
+#var56 me ek STRING value daalni hai
+#fir var 56 mei uss STRING value ko INTEGER mein type cast kare
