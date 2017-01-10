29aeed4616c8be59d4dbcd902ccea373094f5331
vidur <vidur@navgurukul.org>
2017-01-10 14:36:41 +0530

    typo fix

--- list-a.py
+++ list-a.py
@@ -5,7 +5,7 @@
 # KBC ke liye yeh exercise important hai
 
 #question1
-# Ek questions ki LIST banao jiska naam questions ho
+# Ek questions ki LIST banao jiska naam "questions" ho
 
 
 
@@ -15,10 +15,10 @@
 #question2
 # Har questions ke chaar options hote hai
 # 4 aisi LISTS banani hai jinke ITEMS inn questions ke options hai
-# Pehli LIST ke ITEMS sabhi questions ke pehle options honge, iss list ka naam pehle_options hona chaiye
-# Doosri LIST ke ITEMS sabhi questions ke doosre options honge, iss list ka naam dusre_options hona chaiye
-# Teesri LIST ke ITEMS sabhi questions ke teesre options honge, iss list ka naam teesre_options hona chaiye
-# Chauthi LIST ke ITEMS sabhi questions ke chauthe options honge, iss list ka naam chauthe_options hona chaiye
+# first LIST ke ITEMS sabhi questions ke pehle options honge, iss list ka naam "pehle_options" hona chaiye
+# second LIST ke ITEMS sabhi questions ke doosre options honge, iss list ka naam "dusre_options" hona chaiye
+# third LIST ke ITEMS sabhi questions ke teesre options honge, iss list ka naam "teesre_options" hona chaiye
+# fourth LIST ke ITEMS sabhi questions ke chauthe options honge, iss list ka naam "chauthe_options" hona chaiye
 
 
 
@@ -28,7 +28,8 @@
 
 
 #question3
-# Ek LIST banao jiska naam ho saare_options jiske ITEMS pehle_options,dusre_options,teesre_options aur chauthe_options wali list ho
+# Ek LIST banao jiska naam ho "saare_options" jiske ITEMS 
+# "pehle_options","dusre_options","teesre_options" aur "chauthe_options" wali list ho
 
 
 
@@ -38,7 +39,7 @@
 
 
 #question4
-# Ek LIST banao jiske ITEMS answer key hogi, iss list ka naam ans_key hona chaiye
+# Ek LIST banao jiske ITEMS answer key hogi, iss list ka naam "ans_key" hona chaiye
 # Answer Key - Agar answer pehla option hai toh 0, doosra option hai toh 1, teesra hai toh 2, and chautha hai 3
 
 
