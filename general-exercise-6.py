5394547d4e3e582b446496e6c06428837a24fb04
Rishabh Verma <me@rishabhverma.me>
2017-01-16 00:59:34 +0530

    general-exercise-6 half done.

---
+++ general-exercise-6.py
@@ -0,0 +1,38 @@
+# Yeh exercise hai
+# Har line of code se pehle aapko ek line mei comment  mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+
+# Question 1
+# Google jaise bade search engine ek web page pe saare text ko samajhte hain.
+# Fir aap kuch bhi search karo, toh google sahi webpages dikha deta hai.
+# Inn sab kaam ke liye google ko ek page se saare words nikalne padte hain.
+# Hume pehle ek pure paragraph mein se words nikalne hain.
+# Iske liye pehle strings mein se hume words nikalne padenge
+# Socho aapke paas ek string hai
+sentence = "NavGurukul is an alternative to higher education reducing the barriers of current formal education system"
+# Aapne ek `break_into_words` naam ka function likhna hai, jo ek parameter lega jisme ek string hoga
+# Isko jab hum ek words ka string denge. Jaise break_into_words(sentence) doge toh usko ek words ki list return karni hai
+# Jaise aapke `break_into_words(sentence)` ki output yeh honi chaiye:
+# ['NavGurukul', 'is', 'an', 'alternative', 'to', 'higher', 'education', 'reducing', 'the', 'barriers', 'of', 'current', 'formal', 'education', 'system']
+# Hint:
+# Python mein strings bhi list ki tarah hote hain
+# Jaise
+words = "navgurukul is great"
+for i in words:
+    print i
+# Iss loop mein har iteration mein i ki value ek ek character se update hogi
+# Ek baar iss code ko python visualizer mein run kar ke dhang se samajh lo.
+
+
+
+
+
+
+
+
+
+
+
+# Question 2
+# Ab aapne ek paragraph ko alag alag words 
