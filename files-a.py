d526674f3fb4aaeed6d0d55493e9d8805c019437
Rishabh Verma <me@rishabhverma.me>
2017-01-31 13:03:30 +0530

    files exercises.

---
+++ files-a.py
@@ -0,0 +1,82 @@
+# Yeh exercise hai
+# Har line of code se pehle aapko ek line mei comment mei daal kar likhna hai, ki uss line of code ka matlab kya hai
+# Aap jyada comments bhi likh sakte hai, jitne jyada comments likhenge, utna aapkya fayda hoga
+
+# Question 1
+# Iss link (http://navgurukul.org/python/people1-exercise.txt) se file download karo.
+# Ab apne code se iss file ko kholo aur fir, uske saare contents ko print karo.
+
+
+
+
+
+
+
+
+
+# Question 2
+# Aapne jo pichle question mein file download kari hai, usko read karke usme number
+# of lines count kar ke print karein. Aapne sirf uss file ki number of lines Count
+# karne ka code likhna hai.
+# Hint: Har "\n" ke baad nayi line shuru hoti hai. Aap yeh use kar ke nayi line ka
+#       ko pehchan sakte ho.
+
+
+
+
+
+
+
+
+
+
+# Question 3
+# Aapke paas ek list hai. Iss list mein har string ko ek `file-question3.txt`
+# naam ki file mein nayi line mein daalo. Aapki list yeh rahi
+banks_list = ["Kotak", "HDFC", "RBL", "SBI", "Bank of Baroda"]
+
+
+
+
+
+
+
+
+
+
+# Question 4
+# Aapne jo Question 1 mein file download kari hai, uss file ko khol ke dekho.
+# Aapko usme har line pe, ek insaan ka naam aur ek sheher ka naam milega.
+# Yeh sheher woh sheher hai jisme woh insaaan rehta hai. Ab aapne ek aisa
+# code likhna hai jo:
+# 1. Delhi mein rehne waale logon ko ek alag file mein daale.
+#    delhi waali file ka naam "delhi.txt" hona chaiye.
+# 2. Shimla mein rehne waale logon ko ek alag file mein daale.
+#    shimla waali file na naam "shimla.txt" hona chaiye
+# 3. Aur saare log jo delhi aur shimla mein nahi rehte, unko ek alag file mein daale.
+#    iss file ka naam "others.txt" hona chaiye
+# Jaise maan lijiye aapke paas yeh data hai:
+"""
+raghu - shimla
+naseer - kanpur
+karthikeyan - delhi
+salma - jaipur
+siddhi - delhi
+deepti - shimla
+"""
+# Toh aapko teen files banani hai. Delhi waali file mein sirf yeh hoga:
+"""
+karthikeyan - delhi
+siddhi - delhi
+"""
+# Shimla waali file mein yeh hoga:
+"""
+raghu - shimla
+deepti - shimla
+"""
+# Others waali file mein yeh hona chaiye:
+"""
+naseer - kanpur
+salma - jaipur
+"""
+# Aapko aisa code likhna hai, jo aisi file mein kitni bhi lines pe chal paye.
