b9e6d354562b437131f71e31c6ef11025ae218f7
vidur <vidursingla13@gmail.com>
2017-05-29 21:42:39 +0530

    minor changes

--- debugging/cipher.1.0.py
+++ debugging/cipher.1.0.py
@@ -25,7 +25,7 @@
 # Cipher wheel with a function for finding an element in a list
 def find_in_list(query, mainlist):
 # this function is used to find the position of the "query" in the "mainlist". If "query" is in the list then it returns its position, otherwise it returns None
-    mainlist_len = len(mainlist)
+    mainlist_len = len(query)
     range_for_loop = range(mainlist_len)
     index = None
     for i in range_for_loop:
